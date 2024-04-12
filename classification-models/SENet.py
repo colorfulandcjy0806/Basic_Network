d1c5fa2afb4e9c22c8a12a0f23ca48921cd057aa
“colorfulandcjy0806” <chenjunyang0806@gmail.com>
2024-04-12 12:47:01 +0800

    add SENet

---
+++ classification-models/SENet.py
@@ -0,0 +1,32 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class SELayer(nn.Module):
+    def __init__(self, channel, reduction=16):
+        super(SELayer, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.fc = nn.Sequential(
+            nn.Linear(channel, channel // reduction, bias=False),
+            nn.ReLU(inplace=True),
+            nn.Linear(channel // reduction, channel, bias=False),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        b, c, _, _ = x.size()
+        y = self.avg_pool(x).view(b, c)
+        y = self.fc(y).view(b, c, 1, 1)
+        return x * y.expand_as(x)
+
+if __name__ == '__main__':
+    # 实例化模型
+    model = SELayer(3)
+
+    input_tensor = torch.randn(1, 3, 224, 224)  # (batch_size, channels, height, width)
+
+    # 前向传播
+    output = model(input_tensor)
+
+    # 打印输出的形状
+    print(output.shape)
